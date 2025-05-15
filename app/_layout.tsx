461bb894c3fb03c921df09e3491b32921cb4189c
Manoel Carvalho <manoelmecat@gmail.com>
2025-05-15 15:44:38 -0300

    layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,25 @@
-import { Alert, Pressable, Text, TextInput, View } from 'react-native'
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack, useRouter } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
-import { Zocial } from '@expo/vector-icons';
+import { useColorScheme } from "@/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: 'index',
+  initialRouteName: "index",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -26,7 +27,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -50,27 +51,20 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
-  const [username, setUsername] = useState("")
-  const route = useRouter()
-
-  const handleClick = async () => {
-    try {
-      await AsyncStorage.setItem('@username', username)
-      setUsername('')
-      route.replace('/(tabs)')
-    } catch (e) {
-      Alert.alert("Erro", "Erro ao salvar")
-    }
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen options={{
-          headerShown: false
-        }} name='index' />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="index"
+        />
       </Stack>
     </ThemeProvider>
   );
